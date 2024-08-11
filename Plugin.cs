19ea9b41f3f7de301eb529301ce298b8e59c3d89
Roger <Roger@Desktop>
2024-08-11 17:08:44 +0200

    Projektdateien hinzufügen.

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace RocketAutomationMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+            foreach (var method in harmony.GetPatchedMethods())
+            {
+                Logger.LogInfo($"[{PluginInfo.PLUGIN_GUID}] patched {method.FullDescription()}");
+            }
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ RocketAvionicsDevicePatch.cs
@@ -0,0 +1,52 @@
+﻿using Assets.Scripts.Objects.Motherboards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using Objects.Rockets;
+
+namespace RocketAutomationMod
+{
+    [HarmonyPatch(typeof(RocketAvionicsDevice))]
+    internal class RocketAvionicsDevicePatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(RocketAvionicsDevice.CanLogicWrite))]
+        public static void CanLogicWrite(LogicType logicType, ref bool __result)
+        {
+            if (logicType == LogicType.ClearMemory)
+            {
+                __result = true;
+            }
+        }
+
+        [HarmonyPatch(nameof(RocketAvionicsDevice.SetLogicValue))]
+        [HarmonyPostfix]
+        public static void SetLogicValue(LogicType logicType, double value)
+        {
+            if (logicType == LogicType.ClearMemory)
+            {
+                SpaceMapNode node = SpaceMapCode.Get((ulong)value);
+                if (node == null || !node.IsAccessible || node.NodeType != NodeType.Generated)
+                {
+                    return;
+                }
+                //check no rocket connected to node
+                if (node.RocketsHere.Count > 0)
+                {
+                    return;
+                }
+
+                foreach (Rocket rocket in Rocket.AllRockets)
+                {
+                    if (rocket.TargetNode == node)
+                    {
+                        return;
+                    }
+                }
+                node.DeRegister();
+            }
+        }
+    }
+}
